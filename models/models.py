a577b0169e91753b3326674625b6ac71783395dd
CarlosC9 <carlosicaraballo1999@gmail.com>
2020-02-18 20:00:17 +0000

    added constraint reclamaciones name no more 6 letters

--- models/models.py
+++ models/models.py
@@ -7,7 +7,15 @@ class reclamaciones(models.Model):
 
     name = fields.Char(string="ID", required=True)
     date = fields.Date(string="Fecha", required=True)
+    description = fields.Text(string="Descripción", required=True)
+    valid = fields.Boolean(string="Válido", required=True)
+    observation = fields.Text(string="Observaciones", required=False)
 
     _sql_constraints = [
         ('name_unique', 'UNIQUE(name)', "El id de la reclamación debe ser unico")
     ]
+
+    @api.constrains('name')
+    def _check_id(self):
+        if len(self.name) > 6:
+            raise models.ValidationError('El id no puede ser mas largo de 6 letras')
